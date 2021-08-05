4d00f34553bf1d650b1bc54d35bb40decb404b70
JofeGonzalez <60555982+JofeGonzalez@users.noreply.github.com>
2021-08-05 18:59:24 -0300

    ICOMM

---
+++ ICOMM/GetApi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace ICOMM
+{
+    public class GetApi
+    {
+        public dynamic Get(string url)
+        {
+            HttpWebRequest myWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            myWebRequest.UserAgent = ".NET Framework Test Client";
+            myWebRequest.Credentials = CredentialCache.DefaultCredentials;
+            myWebRequest.Proxy = null;
+            HttpWebResponse myHttpWebResponse = (HttpWebResponse)myWebRequest.GetResponse();
+            Stream myStream = myHttpWebResponse.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myStream);
+            //Read data
+            string Datos = HttpUtility.HtmlDecode(myStreamReader.ReadToEnd());
+
+            dynamic data = JsonConvert.DeserializeObject(Datos);
+
+            return data;
+        }
+    }
+}
---
+++ ICOMM/Solution.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ICOMM
+{
+    public class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Positive Cases");
+            Console.WriteLine("2. Positive Rate bigger than 30%");
+            Console.WriteLine("Enter the desired option");
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter the Year.");
+                    int year = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the Month.");
+                    int month = int.Parse(Console.ReadLine());
+                    Console.WriteLine(PositiveCases(month, year));
+                    break;
+                case 2:
+                    List<dynamic> listaImprimir = PositivityRate30();
+                    for (int i = 0; i < listaImprimir.Count; i++)
+                    {
+                        Console.WriteLine(listaImprimir[i].ToString());
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Option entered incorrect");
+                    break;
+            }                        
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// Summarize positives cases on a requested year and month
+        /// </summary>
+        /// <param name="month">Month</param>
+        /// <param name="year">Year</param>
+        /// <returns> Integer </returns>
+        public static int PositiveCases(int month, int year)
+        {
+            int sum = 0;
+            GetApi getApi = new GetApi();
+            dynamic respuesta = getApi.Get("https://api.covidtracking.com/v1/us/daily.json");
+            //Formato-> //añomesdia... aaaammddd
+            for (int i = 0; i < respuesta.Count; i++)
+            {
+                int resultado = respuesta[i].date - int.Parse(year.ToString() + "0" + month.ToString() + "00");
+                if (resultado > 0 && resultado < 32)
+                {
+                    sum = sum + int.Parse(respuesta[i].positiveIncrease.ToString());
+                }
+            }
+            return sum;
+        }
+
+        public static List<dynamic> PositivityRate30()
+        {
+            GetApi getApi = new GetApi();
+            dynamic respuesta = getApi.Get("https://api.covidtracking.com/v1/us/daily.json");
+            List<dynamic> lista = new List<dynamic>();
+            for (int i = 0; i < respuesta.Count; i++)
+            {
+                bool resultado = int.Parse(respuesta[i].positiveIncrease.ToString()) > (int.Parse(respuesta[i].negativeIncrease.ToString()) * 0.3);
+                if (resultado)
+                {
+                    lista.Add(respuesta[i]);
+                }
+            }
+            return lista;
+        }
+
+}
+}
